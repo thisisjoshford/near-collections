3879c0f81ac53603fca41e0fd25f6f6794035eab
Josh Ford <thisisjoshford@gmail.com>
2020-07-28 23:31:18 -0700

    comment cleanup

--- src/utils.js
+++ src/utils.js
@@ -7,12 +7,8 @@ const nearConfig = getConfig(process.env.NODE_ENV || 'development')
 export async function initContract() {
   // Initialize connection to the NEAR testnet
   const near = await connect(Object.assign({ deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } }, nearConfig))
-  // Initializing Wallet based Account. It can work with NEAR testnet wallet that
-  // is hosted at https://wallet.testnet.near.org
   window.walletConnection = new WalletConnection(near)
-  // Getting the Account ID. If still unauthorized, it's just empty string
   window.accountId = window.walletConnection.getAccountId()
-  // Initializing our contract APIs by contract name and configuration
   window.contract = await new Contract(window.walletConnection.account(), nearConfig.contractName, {
     viewMethods: ['getData'],
     changeMethods: ['setData'],
