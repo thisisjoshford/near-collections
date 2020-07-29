ba7a1ca963352bf5698b8b10fba02bfd2e767d50
Josh Ford <thisisjoshford@gmail.com>
2020-07-29 16:37:51 -0700

    refactor welcome screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import 'regenerator-runtime/runtime'
 import React, { useState, useEffect } from 'react'
-import { login, logout, onSubmit } from './utils'
+import { logout, onSubmit } from './utils'
 import './global.css'
 
 import getConfig from './config'
 import { fetchStorage } from './api'
+import WelcomeScreen from './components/WelcomeScreen'
 const { networkId } = getConfig(process.env.NODE_ENV || 'development')
 
 export default function App() {
@@ -24,29 +25,7 @@ export default function App() {
     []
   )
 
-  if (!window.walletConnection.isSignedIn()) {
-    return (
-      <main>
-        <h1>Welcome to NEAR!</h1>
-        <p>
-          To make use of the NEAR blockchain, you need to sign in. The button
-          below will sign you in using NEAR Wallet.
-        </p>
-        <p>
-          By default, when your app runs in "development" mode, it connects
-          to a test network ("testnet") wallet. This works just like the main
-          network ("mainnet") wallet, but the NEAR Tokens on testnet aren't
-          convertible to other currencies – they're just for testing!
-        </p>
-        <p>
-          Go ahead and click the button below to try it out:
-        </p>
-        <p style={{ textAlign: 'center', marginTop: '2.5em' }}>
-          <button onClick={login}>Sign in</button>
-        </p>
-      </main>
-    )
-  }
+  if (!window.walletConnection.isSignedIn()) return <WelcomeScreen/>
 
   return (
     <>
---
+++ src/components/ContractData.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ContractData() {
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
