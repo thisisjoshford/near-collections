a71a028fdae682bdd9c1c21d05ed147341a5ac4f
Josh Ford <thisisjoshford@gmail.com>
2020-07-28 20:42:14 -0700

    notation cleanup

--- src/App.js
+++ src/App.js
@@ -7,37 +7,21 @@ import getConfig from './config'
 const { networkId } = getConfig(process.env.NODE_ENV || 'development')
 
 export default function App() {
-  // use React Hooks to store greeting in component state
   const [greeting, setGreeting] = useState()
-
-  // when the user has not yet interacted with the form, disable the button
   const [buttonDisabled, setButtonDisabled] = useState(true)
-
-  // after submitting the form, we want to show Notification
   const [showNotification, setShowNotification] = useState(false)
 
-  // The useEffect hook can be used to fire side-effects during render
-  // Learn more: https://reactjs.org/docs/hooks-intro.html
   useEffect(
     () => {
-      // in this case, we only care to query the contract swhen signed in
       if (window.walletConnection.isSignedIn()) {
-
-        // window.contract is set by initContract in index.js
         window.contract.getGreeting({ accountId: window.accountId })
           .then(greetingFromContract => {
             setGreeting(greetingFromContract)
           })
       }
     },
-
-    // The second argument to useEffect tells React when to re-run the effect
-    // Use an empty array to specify "only run on first render"
-    // This works because signing into NEAR Wallet reloads the page
     []
   )
-
-  // if not signed in, return early with sign-in prompt
   if (!window.walletConnection.isSignedIn()) {
     return (
       <main>
@@ -63,7 +47,6 @@ export default function App() {
   }
 
   return (
-    // use React Fragment, <>, to avoid wrapping elements in unnecessary divs
     <>
       <button className="link" style={{ float: 'right' }} onClick={logout}>
         Sign out
@@ -79,15 +62,11 @@ export default function App() {
           >
             {greeting}
           </label>
-          {' '/* React trims whitespace around tags; insert literal space character when needed */}
+          {' '}
           {window.accountId}!
         </h1>
         <form onSubmit={async event => {
-          // hold onto new user-entered value from React's SynthenticEvent for use after `await` call
           const newGreeting = event.target.elements.greeting.value
-
-          // fire frontend-agnostic submit behavior, including data persistence
-          // look in utils.js to see how this updates data on-chain!
           await onSubmit(event)
 
           // update local `greeting` variable to match persisted value
@@ -130,22 +109,6 @@ export default function App() {
             </div>
           </fieldset>
         </form>
-        <p>
-          Look at that! A Hello World app! This greeting is stored on the NEAR blockchain. Check it out:
-        </p>
-        <ol>
-          <li>
-            Look in <code>src/App.js</code> and <code>src/utils.js</code> – you'll see <code>getGreeting</code> and <code>setGreeting</code> being called on <code>contract</code>. What's this?
-          </li>
-          <li>
-            Ultimately, this <code>contract</code> code is defined in <code>assembly/main.ts</code> – this is the source code for your <a target="_blank" rel="noreferrer" href="https://docs.near.org/docs/roles/developer/contracts/intro">smart contract</a>.</li>
-          <li>
-            When you run <code>yarn dev</code>, the code in <code>assembly/main.ts</code> gets deployed to the NEAR testnet. You can see how this happens by looking in <code>package.json</code> at the <code>scripts</code> section to find the <code>dev</code> command.</li>
-        </ol>
-        <hr />
-        <p>
-          To keep learning, check out <a target="_blank" rel="noreferrer" href="https://docs.near.org">the NEAR docs</a> or look through some <a target="_blank" rel="noreferrer" href="https://examples.near.org">example apps</a>.
-        </p>
       </main>
       {showNotification && <Notification />}
     </>
@@ -160,7 +123,7 @@ function Notification() {
       <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.accountId}`}>
         {window.accountId}
       </a>
-      {' '/* React trims whitespace around tags; insert literal space character when needed */}
+      {' '}
       called method: 'setGreeting' in contract:
       {' '}
       <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.contract.contractId}`}>
--- src/utils.js
+++ src/utils.js
@@ -6,15 +6,12 @@ const nearConfig = getConfig(process.env.NODE_ENV || 'development')
 // Initialize contract & set global variables
 export async function initContract() {
   // Initialize connection to the NEAR testnet
-  const near = await connect(Object.assign({ deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } }, nearConfig))
-
+  const near = await connect(Object.assign({ deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } }, nearConfig))\
   // Initializing Wallet based Account. It can work with NEAR testnet wallet that
   // is hosted at https://wallet.testnet.near.org
   window.walletConnection = new WalletConnection(near)
-
   // Getting the Account ID. If still unauthorized, it's just empty string
   window.accountId = window.walletConnection.getAccountId()
-
   // Initializing our contract APIs by contract name and configuration
   window.contract = await new Contract(window.walletConnection.account(), nearConfig.contractName, {
     // View methods are read only. They don't modify the state, but usually return some value.
@@ -24,21 +21,15 @@ export async function initContract() {
   })
 }
 
-// attached to the form used to update the greeting
-// in utils because it works with a vanilla JS or a React approach
 export async function onSubmit(event) {
   event.preventDefault()
-
-  // get elements from the form using their id attribute
   const { fieldset, greeting } = event.target.elements
 
   // disable the form while the value gets updated on-chain
   fieldset.disabled = true
 
   try {
-    // make an update call to the smart contract
     await contract.setGreeting({
-      // pass the value that the user entered in the greeting field
       message: greeting.value
     })
   } catch (e) {
@@ -56,14 +47,9 @@ export async function onSubmit(event) {
 
 export function logout() {
   window.walletConnection.signOut()
-  // reload page
   window.location.replace(window.location.origin + window.location.pathname)
 }
 
 export function login() {
-  // Allow the current app to make calls to the specified contract on the
-  // user's behalf.
-  // This works by creating a new access key for the user's account and storing
-  // the private key in localStorage.
   window.walletConnection.requestSignIn(nearConfig.contractName)
 }
