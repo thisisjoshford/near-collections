26dec6c6300ff9a289daf755b193c52accfda84b
Josh Ford <thisisjoshford@gmail.com>
2020-07-29 19:52:20 -0700

    conditional data rendering

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ export default function App() {
             </div>
           </fieldset>
         </form>
-        {storage? <AccountData storage={storage}/> : null}
+        {storage?.length > 0 ? <AccountData storage={storage}/> : null}
       </main>
       {showNotification && <Notification />}
     </>
